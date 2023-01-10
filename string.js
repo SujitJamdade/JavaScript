4835626f74a8b6c6db4491b41fcdcb8d92e0c6a3
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2023-01-10 20:22:28 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,34 @@
+// alert('hello, world');
+
+// String
+console.log('hello, world');
+
+let email = 'abc@gmail.com';
+console.log(email);
+
+// String concatation
+let firstName = 'Virendra';
+let lastName = 'Sehwag';
+
+let fullName = firstName +' '+ lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+console.log(fullName[2]);
+
+// String length
+console.log(fullName.length);
+
+// String methods
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLocaleLowerCase();
+console.log(result);
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index)
+
+
+
