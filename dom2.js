4af500b72c69d0bcdb3d39f695b00f5f99b659fd
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2023-01-13 09:22:51 +0530

    Add files via upload

---
+++ dom2.js
@@ -0,0 +1,17 @@
+const copy = document.querySelector('.copy-me');
+
+copy.addEventListener('copy', ()=>{
+    console.log("Hey! my content is copyright!");
+});
+
+const box = document.querySelector('.box');
+
+box.addEventListener('mousemove', e=>{
+    // console.log(e);
+    // console.log(e.offsetX, e.offsetY);
+    box.textContent = `x pos - ${e.offsetX} y pos - ${e.offsetY}`;
+});
+
+document.addEventListener('wheel',e =>{
+    console.log(e.pageX, e.pageY);
+});
